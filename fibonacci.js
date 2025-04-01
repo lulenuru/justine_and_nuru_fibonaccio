ed3d67bce005b5dac831db8af3318a27859e33da
bobnuru <lulenuru21@gmail.com>
2025-04-01 20:53:41 +0300

    test to check fibonacci of any whole number

--- fibonacci.js
+++ fibonacci.js
@@ -2,14 +2,13 @@ export default function fibonacci(number) {
     // base case
     if(number === 0) {
         return 0;
-    }
-
-    if(number === 1) {
+    }else if(number === 1) {
         return 1
-    }
+    }else{
 
     // fibonacci(3) = fibonacci(2) + fibonacci(1)
     return fibonacci(number - 1) + fibonacci(number - 2)
+    }
 }
 
 
--- test/testFibonacci.js
+++ test/testFibonacci.js
@@ -6,5 +6,15 @@ describe("test fibonacci of the first 5 numbers", ()=>{
     it("test fibonacci(1) to see if it returns 1", ()=>{
         assert.strictEqual(fibonacci(1),1)
     })
+
+    it("test fibonacci() any other number", ()=>{
+        assert.strictEqual(fibonacci(8),21)
+    })
+   
+    it("test fibonacci() any other number", ()=>{
+        assert.strictEqual(fibonacci(10),55)
+    })
    
 })
+
+// Nuru Lule & Justine Mbawomye
